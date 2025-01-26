42d5d5416c92b6254099315564aaf74c06dc9c86
regisksc <41172158+regisksc@users.noreply.github.com>
2025-01-26 16:35:41 -0300

    Initial Commit

---
+++ StateManagementPoc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateManagementPoc
+//
+//  Created by Regis Kian on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateManagementPoc/StateManagementPocApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementPocApp.swift
+//  StateManagementPoc
+//
+//  Created by Regis Kian on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementPocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
